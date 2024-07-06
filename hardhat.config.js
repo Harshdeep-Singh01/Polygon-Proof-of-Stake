96afc1e3fdf626ca39d78d1007a5109e4faccdb0
Harshdeep-Singh01 <140712650+Harshdeep-Singh01@users.noreply.github.com>
2024-07-06 18:42:32 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,14 +5,14 @@ module.exports = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/7a450d4492094e3b991f70855552ddb9",
+      url: "https://sepolia.infura.io/v3/process.env.INFURA_API_KEY",
       chainId: 11155111,
-      accounts: ["2383258e510adc86fa59f266948cc62a82f0335b70dff9a81dcea2f8b694a1fe"]
+      accounts: [process.env.PRIVATE_KEY]
     },
     polygon_amoy: {
       url: "https://rpc-amoy.polygon.technology/",
       chainId: 80002,
-      accounts: ["2383258e510adc86fa59f266948cc62a82f0335b70dff9a81dcea2f8b694a1fe"]
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
 };
